391e5ec803cafe045624c49f5d1f605eac0570f9
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-08-25 08:56:39 +0300

    update open graph configuration for displaying og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,29 @@ export async function generateMetadata(): Promise<Metadata> {
     title: siteConfig.name,
     description: siteConfig.description,
     generator: "v0.app",
+    openGraph: {
+      title: siteConfig.name,
+      description: siteConfig.description,
+      url: siteConfig.url,
+      siteName: siteConfig.name,
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig.name,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: siteConfig.name,
+      description: siteConfig.description,
+      images: [siteConfig.ogImage],
+      creator: siteConfig.author.twitter,
+    },
     icons: {
       icon: [
         { 
